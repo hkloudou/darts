ba6870f643688d3306b18666ffa9a5d2823bca27
sample <global-config@example.com>
2022-05-19 12:29:18 +0800

    remore uikits

--- flutter_uikits/lib/flutter_uikits.dart
+++
@@ -1,6 +0,0 @@
-library flutter_uikits;
-
-export './widgets/banner/simple/simple_banner_watcher.dart'
-    show SimpleBannerWatcher;
-export './widgets/notification/verticalmarquee/vertical_marquee_notification_watcher.dart'
-    show VerticalMarqueeNotificationWatcher;
